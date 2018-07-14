16adbcfe0f8c0502b0c2920e3fe2fc428358957d
Rickyip <13002066d@connect.polyu.hk>
2018-07-14 15:29:54 +0800

    Code refactoring

--- TwitterRebuild/AppDelegate.swift
+++ TwitterRebuild/AppDelegate.swift
@@ -16,12 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        
-        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
-        
         window?.rootViewController = UINavigationController(rootViewController: homeController)
         
         return true
