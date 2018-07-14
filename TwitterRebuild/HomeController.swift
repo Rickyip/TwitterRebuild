cc6208bcf301965849b7e68fc59a3643436c8c29
Rickyip <13002066d@connect.polyu.hk>
2018-07-14 15:26:55 +0800

    Initial Commit

---
+++ TwitterRebuild/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  TwitterRebuild
+//
+//  Created by RickYip on 14/7/2018.
+//  Copyright © 2018年 RickYip. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
